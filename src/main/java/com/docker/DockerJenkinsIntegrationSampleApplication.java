445e3a05116d2b32e399706bf558bd0db81f6c03
Sivalingam <sivalingam160599@gmail.com>
2022-08-01 10:04:28 +0530

    first commit

---
+++ src/main/java/com/docker/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,14 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
