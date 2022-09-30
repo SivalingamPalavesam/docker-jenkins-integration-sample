049a8b639bb0f3f95e585d4da85c618bdedfa417
Sivalingam <sivalingam160599@gmail.com>
2022-09-30 17:14:31 +0530

    Multiple Data Source Connected

--- src/main/java/com/docker/entity/postgres/Account.java
+++ src/main/java/com/docker/entity/postgres/Account.java
@@ -1,44 +1,68 @@
-/*
- * package com.docker.entity.postgres;
- * 
- * import javax.persistence.Column; import javax.persistence.Entity; import
- * javax.persistence.Id; import javax.persistence.Table;
- * 
- * @Entity
- * 
- * @Table(name="account") public class Account {
- * 
- * @Id
- * 
- * @Column(name = "id") private Integer id;
- * 
- * @Column(name = "balance") private int balance;
- * 
- * @Column(name = "account_name") private String accountName;
- * 
- * @Column(name = "date_opened") private String dateOpened;
- * 
- * public Account() { super(); }
- * 
- * public Account(Integer id, int balance, String accountName, String
- * dateOpened) { super(); this.id = id; this.balance = balance; this.accountName
- * = accountName; this.dateOpened = dateOpened; }
- * 
- * public Integer getId() { return id; }
- * 
- * public void setId(Integer id) { this.id = id; }
- * 
- * public int getBalance() { return balance; }
- * 
- * public void setBalance(int balance) { this.balance = balance; }
- * 
- * public String getAccountName() { return accountName; }
- * 
- * public void setAccountName(String accountName) { this.accountName =
- * accountName; }
- * 
- * public String getDateOpened() { return dateOpened; }
- * 
- * public void setDateOpened(String dateOpened) { this.dateOpened = dateOpened;
- * } }
- */
+package com.docker.entity.postgres;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="account")
+public class Account {
+
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "balance")
+	private int balance;
+
+	@Column(name = "account_name")
+	private String accountName;
+
+	@Column(name = "date_opened")
+	private String dateOpened;
+
+	public Account() {
+		super();
+	}
+
+	public Account(Integer id, int balance, String accountName, String dateOpened) {
+		super();
+		this.id = id;
+		this.balance = balance;
+		this.accountName = accountName;
+		this.dateOpened = dateOpened;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getDateOpened() {
+		return dateOpened;
+	}
+
+	public void setDateOpened(String dateOpened) {
+		this.dateOpened = dateOpened;
+	}
+}
--- src/main/java/com/docker/repository/postgres/PostgresCustomerRepository.java
+++ src/main/java/com/docker/repository/postgres/PostgresCustomerRepository.java
@@ -1,14 +1,12 @@
-/*
- * package com.docker.repository.postgres;
- * 
- * import org.springframework.data.jpa.repository.JpaRepository; import
- * org.springframework.stereotype.Repository;
- * 
- * import com.docker.entity.postgres.Account;
- * 
- * 
- * @Repository public interface PostgresCustomerRepository extends
- * JpaRepository<Account, Integer> {
- * 
- * }
- */
+package com.docker.repository.postgres;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.docker.entity.postgres.Account;
+
+
+@Repository
+public interface PostgresCustomerRepository extends JpaRepository<Account, Integer> {	
+	
+}
